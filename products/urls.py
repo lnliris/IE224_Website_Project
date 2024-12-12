46ff0f655a65c6d141e7bcc8f431ce9da591adde
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2024-12-12 23:52:14 +0700

    Thêm trường slg và variant, truy xuất được dữu liệu

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import ProductsView
+from . import views
 
 urlpatterns = [
-    path('products/', ProductsView.as_view(), name='products'), 
+    # path('products/', ProductsView.as_view(), name='products'),
+    path('', views.product_list, name='product_list'),  # Danh sách sản phẩm
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),  # Theo danh mục
+    path('product/<slug:product_slug>/', views.product_detail, name='product_detail'),  # Chi tiết sản phẩm
 ]
