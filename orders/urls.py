1daee9247ccf5419f9c25577ee40584aa2127580
lyle0409 <lyle049036@gmail.com>
2024-12-17 23:19:36 +0700

    add checkout order model

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('order-summary/', views.order_summary, name='order-summary'),
     path('payment/', views.payment_view, name='payment'),
     path('order-history/', views.order_history, name='order-history'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render
 from .models import Order, OrderHistory
+from cart import models, views
+from cart.models import Cart, CartItem 
 
 def order_summary(request):
     order = Order.objects.get(user=request.user, ordered=False)
@@ -17,3 +19,7 @@ def payment_view(request):
 def order_history(request):
     history = OrderHistory.objects.filter(user=request.user).order_by('-updated_at')
     return render(request, 'order_history.html', {'history': history})
+
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)
