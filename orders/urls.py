5b0cc1047a9f073cfc0f019a6abacfed734daef8
lyle0409 <lyle049036@gmail.com>
2024-12-18 11:07:52 +0700

    checkout form

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('order-summary/', views.order_summary, name='order-summary'),
+    # path('order-summary/', views.order_summary, name='order-summary'),
     path('payment/', views.payment_view, name='payment'),
     path('order-history/', views.order_history, name='order-history'),
     path('checkout/', views.checkout, name='checkout'),
